7e8b62f10f341560e9ef141ff65c2a924770fcfd
Trieu192 <luongquoctrieu165@gmail.com>
2024-05-16 23:48:56 +0700

    Add new startIcon into the productdetails but doesn't complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,25 +13,27 @@ import {
 import { Link, useNavigate } from "react-router-dom";
 
 export default function Home() {
-
   const navigate = useNavigate();
   const data = [
     {
       id: "1",
       title: "pen",
       price: "5.000",
+      rating: "3.5",
       image: "https://m.media-amazon.com/images/I/71XmnK4NmXL._AC_SL1500_.jpg",
     },
     {
       id: "2",
       title: "eraser",
       price: "2.000",
+      rating: "3",
       image: "https://m.media-amazon.com/images/I/716YBLPCc7L.jpg",
     },
     {
       id: "3",
       title: "phone",
       price: "20.000.000",
+      rating: "2.5",
       image:
         "https://media.wired.com/photos/5b22c5c4b878a15e9ce80d92/master/pass/iphonex-TA.jpg",
     },
@@ -39,6 +41,7 @@ export default function Home() {
       id: "4",
       title: "papers",
       price: "50.000",
+      rating: "3.5",
       image:
         "https://www.themandarin.com.au/wp-content/uploads/2022/12/journal-articles-papers.jpg?w=795",
     },
@@ -46,6 +49,7 @@ export default function Home() {
       id: "5",
       title: "fan",
       price: "175.000",
+      rating: "3.5",
       image:
         "https://m.media-amazon.com/images/I/81PL5OvB13L._AC_UF894,1000_QL80_DpWeblab_.jpg",
     },
@@ -53,6 +57,7 @@ export default function Home() {
       id: "6",
       title: "charger",
       price: "250.000",
+      rating: "3.5",
       image:
         "https://m.media-amazon.com/images/I/61NfFCwsneL._AC_UF1000,1000_QL80_.jpg",
     },
@@ -60,12 +65,14 @@ export default function Home() {
       id: "7",
       title: "wifi router",
       price: "500.000",
+      rating: "3.5",
       image: "https://m.media-amazon.com/images/I/51R2a9p-vNL._AC_SL1000_.jpg",
     },
     {
       id: "8",
       title: "wallet",
       price: "50.000",
+      rating: "3.5",
       image:
         "https://m.media-amazon.com/images/I/71SPlw9sSUL._AC_UY1000_DpWeblab_.jpg",
     },
@@ -73,6 +80,7 @@ export default function Home() {
       id: "9",
       title: "headphones",
       price: "100.000",
+      rating: "3.5",
       image:
         "https://m.media-amazon.com/images/I/41tp0JPPlmL._AC_UF894,1000_QL80_.jpg",
     },
@@ -80,11 +88,11 @@ export default function Home() {
       id: "10",
       title: "macbook",
       price: "100.000.000",
+      rating: "3.5",
       image:
         "https://cdn.tgdd.vn/Products/Images/44/231244/macbook-air-m1-2020-gray-600x600.jpg",
     },
   ];
-
 
   return (
     <Grid container>
@@ -101,29 +109,31 @@ export default function Home() {
         >
           {data.map((item) => (
             <Card key={item.id} sx={{ maxWidth: 200, minWidth: 345 }}>
-              
-                <CardActionArea onClick={()=>{navigate(`products/${item.id}`, { state: item }) }}>
-                  <CardMedia
-                    component="img"
-                    height="200"
-                    image={`${item.image}?w=150&h=150&fit=crop&auto=format`}
-                    alt="laptop"
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h4" component="div">
-                      {item.title}
-                    </Typography>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {item.price} vnd
-                    </Typography>
-                    {/* <Typography variant="body2" color="text.secondary">
+              <CardActionArea
+                onClick={() => {
+                  navigate(`products/${item.id}`, { state: item });
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  height="200"
+                  image={`${item.image}?w=150&h=150&fit=crop&auto=format`}
+                  alt="laptop"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h4" component="div">
+                    {item.title}
+                  </Typography>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {item.price} vnd
+                  </Typography>
+                  {/* <Typography variant="body2" color="text.secondary">
                       Lizards are a widespread group of squamate reptiles, with
                       over 6,000 species, ranging across all continents except
                       Antarctica
                     </Typography> */}
-                  </CardContent>
-                </CardActionArea>
-              
+                </CardContent>
+              </CardActionArea>
             </Card>
           ))}
         </ImageList>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,17 +1,54 @@
-import { Grid, Typography } from "@mui/material";
-
+import { Grid, Typography, Box } from "@mui/material";
+import StarIcon from "@mui/icons-material/Star";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
 import { useLocation } from "react-router-dom";
 export default function Product() {
   let location = useLocation();
 
+  const getStarColor = (index, rating) => {
+    // Change the color intensity based on the rating
+    const color = index < rating ? "gold" : "gray";
+    return color;
+  };
+
+  const renderStars = (rating, maxRating = 5) => {
+    return (
+      <Box display="flex" alignItems="center">
+        {Array.from({ length: maxRating }, (_, index) =>
+          index < rating ? (
+            <StarIcon
+              key={index}
+              style={{ color: getStarColor(index, rating) }}
+            />
+          ) : (
+            <StarBorderIcon
+              key={index}
+              style={{ color: getStarColor(index, rating) }}
+            />
+          )
+        )}
+      </Box>
+    );
+  };
   console.log("location", location.state);
   return (
     <Grid container>
       <Typography variant="h6">Product detail</Typography>
       <Grid item>
-        <img  src={location.state.image} alt={location.state.title} />
-        <Typography variant="h2">{location.state.title}</Typography>
-        <Typography variant="h3">{location.state.price}</Typography>
+        <Box display="flex" flexDirection="row" alignItems="center">
+          <img
+            src={location.state.image}
+            alt={location.state.title}
+            style={{ maxWidth: "50%", height: "auto" }}
+          />
+          <Box display="flex" flexDirection="column" marginLeft={2}>
+            <Typography variant="h2">{location.state.title}</Typography>
+            {renderStars(location.state.rating)}
+            <Typography variant="h3">{location.state.price}</Typography>
+
+            {/* Add the star rating here */}
+          </Box>
+        </Box>
       </Grid>
     </Grid>
   );
