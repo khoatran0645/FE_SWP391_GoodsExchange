836826ef0f1d03adc3427dfdf31ed6af62acc5e0
PREDATOR\khoatm <khoatran0645@gmail.com>
2024-06-19 12:38:55 +0700

    clean project and edit Delete Product API

--- src/constant.js
+++ src/constant.js
@@ -8,8 +8,8 @@
 export const BE_BASE_URL = "http://localhost:5000/api/v1";
 // export const BE_BASE_URL = "https://goodexchange-swp.azurewebsites.net/api/v1";
 
-
 //* Category APIs
+//DONE
 export const API_CREATE_CATEGORY = "/categories";
 export const API_GET_ALL_CATEGORIES = "/categories";
 export const API_UPDATE_CATEGORY = "/categories";
@@ -27,7 +27,7 @@ export const API_GET_PRODUCTS_HOMEPAGE =
 export const API_SEARCH_PRODUCTS_FOR_USER =
   "/products/all?PageIndex=1&PageSize=10&ProductName={keyword}&IsApproved=true&IsActive=true";
 //! DANGER
-export const API_DELETE_PRODUCT = "/products/{id}";
+export const API_DELETE_PRODUCT = "/products/id?id={id}";
 
 //* Rating APIs
 //TODO create, get, update, hide rating
@@ -37,7 +37,6 @@ export const API_DELETE_PRODUCT = "/products/{id}";
 export const API_POST_REPORT = "/reports/sendreport";
 
 export const API_GET_ALL_REPORTS = "/reports/all?PageIndex=1&PageSize=10";
-
 
 //* User APIs
 //TODO login, register, update user info, ban account, get seller profile
