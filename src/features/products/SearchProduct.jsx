e9c1a93e735d069b81561c6caab0e8c956c7e8da
PREDATOR\khoatm <khoatran0645@gmail.com>
2024-06-25 17:50:10 +0700

    add ProductCard to reuse

--- src/features/products/SearchProduct.jsx
+++ src/features/products/SearchProduct.jsx
@@ -1,20 +1,8 @@
-import React from "react";
 import useStore from "../../app/store";
-import {
-  Grid,
-  Typography,
-  Container,
-  Box,
-  ImageList,
-  Card,
-  CardContent,
-  CardMedia,
-  CardActionArea,
-} from "@mui/material";
+import { Grid, Typography, ImageList } from "@mui/material";
 
-import { Link, useNavigate } from "react-router-dom";
+import ProductCard from "./ProductCard";
 function SearchProduct() {
-  const navigate = useNavigate();
   const searchResult = useStore((state) => state.searchResult);
   console.log(searchResult);
   return (
@@ -30,33 +18,7 @@ function SearchProduct() {
         // rowHeight={164}
       >
         {searchResult?.items.map((item) => (
-          <Card key={item.productId} sx={{ maxWidth: 345, minWidth: 200 }}>
-            <CardActionArea
-              onClick={() => {
-                navigate(`/products/${item.productId}`, { state: item });
-              }}
-            >
-              <CardMedia
-                component="img"
-                height="200"
-                image={`${item.productImageUrl}?w=150&h=150&fit=crop&auto=format`}
-                alt={item.productName}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {item.productName}
-                </Typography>
-                <Typography gutterBottom variant="h6" component="div">
-                  {item.price} VND
-                </Typography>
-                {/* <Typography variant="body2" color="text.secondary">
-                      Lizards are a widespread group of squamate reptiles, with
-                      over 6,000 species, ranging across all continents except
-                      Antarctica
-                    </Typography> */}
-              </CardContent>
-            </CardActionArea>
-          </Card>
+          <ProductCard key={item.productId} item={item} />
         ))}
       </ImageList>
     </>
