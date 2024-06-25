541ff68a3b4f7e9ec2a4ffb3f69a2efe58077447
PREDATOR\khoatm <khoatran0645@gmail.com>
2024-06-25 18:07:09 +0700

    hot fix Profile

--- src/features/profile/Profile.jsx
+++ src/features/profile/Profile.jsx
@@ -1,5 +1,4 @@
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   Avatar,
   Paper,
@@ -12,285 +11,101 @@ import {
   Tabs,
   Tab,
 } from "@mui/material";
+import { useEffect } from "react";
 
 import NavBar from "../common/NavBar";
-
-import useStore from "../../app/store";
-
-import { jwtDecode } from "jwt-decode";
-import { red } from "@mui/material/colors";
-
-function Profile() {
-  const getProfileUserById = useStore((state) => state.getProfileUserById);
-
-  const userInfo = useStore.getState().userInfo;
-
-  const userDetail = jwtDecode(userInfo.data.token);
-
-  // const Userprofile = useState.getState().userDetails;
-
-  console.log("userDetail : ", userDetail);
-
-  useEffect(() => {
-    getProfileUserById(userDetail.id);
-  }, []);
-
-  const profileDetail = useStore((state) => state.userProfile);
-
-  console.log("profileDetail  ", profileDetail);
-
-  // const [formData, setFormData] = useState(profileData);
-
-  const [selectedMenu, setSelectedMenu] = useState("Thông tin cá nhân");
-
-  const menuItems = [
-    { text: "Thông tin cá nhân", key: "Thông tin cá nhân" },
-
-    { text: "Cài đặt tài khoản", key: "Cài đặt tài khoản" },
+import { useNavigate } from "react-router-dom";
+const Profile = () => {
+   const navigate = useNavigate();
+  const data = [
+    {
+      lastName: "Phuong",
+      firstName: "Thao",
+      phone: 88888888,
+      image:
+        "https://i.pinimg.com/originals/94/ba/f3/94baf36bb56658bfe8dfba142e4a98a3.jpg",
+    },
   ];
-  const handleChange = (e) => {
-    const { id, value } = e.target;
-
-    setFormData((prevData) => ({
-      ...prevData,
-
-      [id]: value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    console.log(formData);
-
-    onProfileSubmit(formData);
-  };
-
-  return (
-    <>
-      <NavBar />
-      <Box sx={{ display: "flex", height: "100vh" }}>
-        {/* Menu sidebar */}
-        <Box sx={{ width: "250px", borderRight: "1px solid #ddd" }}>
-          <List component="nav">
-            {menuItems.map((item) => (
-              <div key={item.key}>
-                <ListItem
-                  onClick={() => setSelectedMenu(item.key)}
-                  sx={{
-                    backgroundColor:
-                      selectedMenu === item.key
-                        ? "rgba(0, 0, 255, 0.1)"
-                        : "inherit",
-                  }}
-                >
-                  <ListItemText primary={item.text} />
-                </ListItem>
-
-                <Divider />
-              </div>
-            ))}
-          </List>
-        </Box>
-        {/* form  */}
-        <Box
-          component="form"
-          sx={{
-            "& .MuiTextField-root": { m: 1, width: "45ch" },
-
-            display: "flex",
-
-            flexDirection: "column",
-
-            maxWidth: "600px",
-            maxHeight: "600px",
-            alignItems: "center",
-            justifyContent: "center",
-            margin: "10px 100px",
-
-            padding: "20px",
-
-            boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-
-            borderRadius: "8px",
-          }}
-          noValidate
-          autoComplete="off"
-          onSubmit={handleSubmit}
-        >
-          <h2>Thông tin cá nhân</h2>
-
-          <TextField
-            required
-            id="name"
-            label="Họ và tên"
-            // defaultValue="Thảo Phương"
-            value={profileDetail?.fullName}
-            onSubmit={handleSubmit}
-          />
-
-          <TextField
-            required
-            id="phone"
-            value={profileDetail?.phoneNumber}
-            label="Thêm số điện thoại"
-          />
-
-          <TextField
-            id="email"
-            label="Email"
-            value={profileDetail?.email}
-            InputProps={{
-              readOnly: true,
-            }}
-          />
-
-          {/* <TextField id="cccd" label="CCCD / CMND / Hộ chiếu" />
-
-      <TextField id="invoice-info" label="Thông tin xuất hoá đơn" />
-
-      <TextField id="tax-code" label="Mã số thuế" /> */}
-
-          {/* <TextField id="favorite-category" label="Danh mục yêu thích" /> */}
-
-          <TextField
-            id="birthday"
-            label="Ngày, tháng, năm sinh"
-            type="date"
-            // defaultValue={"2003-09-19"}
-            value={profileDetail?.dateOfBirth}
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-
-          <Button variant="contained" color="primary" sx={{ mt: 2 }}>
-            Lưu thay đổi
-          </Button>
-        </Box>
-      </Box>
-    </>
-  );
-}
-
-function App() {
-  const initialProfileData = {
-    name: "Thảo Phương",
-
-    phone: "",
-
-    address: "",
-
-    introduction: "",
-
-    nickname: "",
-
-    email: "phuongthao0322003@gmail.com",
-
-    cccd: "",
-
-    invoiceInfo: "",
-
-    taxCode: "",
-
-    favoriteCategory: "",
-
-    gender: "Nữ",
-
-    birthday: "",
-  };
-
-  // const getProfileUserById = useStore((state) => state.getProfileUserById);
-
-  // const userInfo = useStore.getState().userInfo;
-
-  // const userDetail = jwtDecode(userInfo.data.token);
-
-  // // const Userprofile = useState.getState().userDetails;
-
-  // console.log("userDetail : ", userDetail.id);
-
-  // useEffect(() => {
-
-  //   getProfileUserById(userDetail.id);
-
-  // }, []);
-
-  // const profileDetail = useStore((state) => state.userDetails);
-
-  // console.log("profileDetail  ", profileDetail);
-
-  // const userDetails = useStore.getState().userDetails;
-
-  // console.log("userDetails", userDetails);
-
-  // console.log("userid", userDetail.id);
-
-  const [profileData, setProfileData] = useState(userDetail);
-
-  const [selectedMenu, setSelectedMenu] = useState("Thông tin cá nhân");
-
-  const menuItems = [
-    { text: "Thông tin cá nhân", key: "Thông tin cá nhân" },
-
-    { text: "Cài đặt tài khoản", key: "Cài đặt tài khoản" },
-  ];
-
-  const handleProfileSubmit = (newData) => {
-    setProfileData(newData);
-
-    setSelectedMenu("Thông tin cá nhân");
-  };
+  const [in4List, setIn4List] = React.useState(data);
 
   return (
     <>
       <NavBar />
 
-
-      <Box sx={{ display: "flex", height: "100vh" }}>
-        <Box sx={{ width: "250px", borderRight: "1px solid #ddd" }}>
-          <List component="nav">
-            {menuItems.map((item) => (
-              <div key={item.key}>
-                <ListItem
-                  onClick={() => setSelectedMenu(item.key)}
-                  sx={{
-                    backgroundColor:
-                      selectedMenu === item.key
-                        ? "rgba(0, 0, 255, 0.1)"
-                        : "inherit",
-                  }}
+      <Grid container spacing={2} padding={3}>
+        <Grid item xs={12} md={4}>
+          <Box
+            sx={{
+              p: 2,
+              border: "1px solid #e0e0e0",
+              borderRadius: 2,
+              textAlign: "center",
+              position: "relative",
+              bgcolor: "#fafafa",
+            }}
+          >
+            <Box sx={{ position: "relative", display: "inline-block" }}>
+              <Badge
+                overlap="circular"
+                anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                badgeContent={
+                  <IconButton
+                    sx={{
+                      bgcolor: "white",
+                      border: "1px solid #e0e0e0",
+                      padding: "2px",
+                    }}
+                  ></IconButton>
+                }
+              >
+                <Avatar
+                  alt="Psyduck"
+                  src="https://i.pinimg.com/originals/94/ba/f3/94baf36bb56658bfe8dfba142e4a98a3.jpg"
+                  sx={{ width: 100, height: 100 }}
+                />
+              </Badge>
+            </Box>
+            {in4List.map((in4) => (
+              <>
+                <Typography variant="h6" sx={{ mt: 1 }}>
+                  {in4.lastName} {in4.firstName}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  sx={{ mt: 1 }}
                 >
-                  <ListItemText primary={item.text} />
-                </ListItem>
-
-                <Divider />
-              </div>
+                  Phone: {in4.phone}
+                </Typography>
+              </>
             ))}
-          </List>
-        </Box>
+            <Typography variant="body2" color="textSecondary">
+              Chưa có đánh giá
+            </Typography>
 
-        <Box sx={{ flex: 1, padding: "20px" }}>
-          {selectedMenu === "Thông tin cá nhân" && (
-            <Profile
-              profileData={profileData}
-              onProfileSubmit={handleProfileSubmit}
-            />
-          )}
+            <Button variant="outlined" fullWidth sx={{ mt: 1 }} onClick={() => navigate('/edit-profile')}>
+              Chỉnh sửa trang cá nhân
+            </Button>
+            <Typography variant="body2" color="textSecondary" sx={{ mt: 2 }}>
+              Phản hồi chat: Chưa có thông tin
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              Đã tham gia: 1 ngày
+            </Typography>
+          </Box>
+        </Grid>
 
-          {selectedMenu !== "Thông tin cá nhân" && (
-            <Box
-              sx={{
-                display: "flex",
-
-                justifyContent: "center",
-
-                alignItems: "center",
-
-                height: "100%",
-              }}
-            >
-              <h2>{selectedMenu}</h2>
+        <Grid item xs={12} md={8}>
+          <Paper sx={{ padding: 2, height: "100%" }}>
+            <Tabs value={0} textColor="secondary" indicatorColor="secondary">
+              <Tab label="Đang hiển thị (0)" />
+              <Tab label="Đã bán (0)" />
+            </Tabs>
+            <Box sx={{ textAlign: "center", marginTop: 5 }}>
+              <Typography variant="h6">Bạn chưa có tin đăng nào</Typography>
+              <Button variant="contained" color="warning" sx={{ marginTop: 2 }}>
+                ĐĂNG TIN NGAY
+              </Button>
             </Box>
           </Paper>
         </Grid>
