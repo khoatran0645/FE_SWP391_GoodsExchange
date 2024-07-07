2e40e79a4629d2e51c0d3a596bd808a8874f8461
REICHAN\phuon <thaottpse173309@fpt.edu.vn>
2024-07-07 17:50:37 +0700

    update button change psw

--- src/features/profile/EditProfile.jsx
+++ src/features/profile/EditProfile.jsx
@@ -9,6 +9,8 @@ import {
   ListItem,
   ListItemText,
   Divider,
+  Typography,
+  Modal,
 } from "@mui/material";
 
 import NavBar from "../common/NavBar";
@@ -17,6 +19,18 @@ import useStore from "../../app/store";
 
 import { jwtDecode } from "jwt-decode";
 import { red } from "@mui/material/colors";
+
+const resetPasswordModalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 function Profile() {
   const getProfileUserById = useStore((state) => state.getProfileUserById);
@@ -41,11 +55,7 @@ function Profile() {
 
   const [selectedMenu, setSelectedMenu] = useState("Thông tin cá nhân");
 
-  const menuItems = [
-    { text: "Thông tin cá nhân", key: "Thông tin cá nhân" },
-
-    { text: "Cài đặt tài khoản", key: "Cài đặt tài khoản" },
-  ];
+  const menuItems = [{ text: "Thông tin cá nhân", key: "Thông tin cá nhân" }];
   const handleChange = (e) => {
     const { id, value } = e.target;
 
@@ -63,6 +73,10 @@ function Profile() {
 
     onProfileSubmit(formData);
   };
+
+  const [openResetPasswordModal, setOpenResetPasswordModal] = useState(false);
+  const handleOpen = () => setOpenResetPasswordModal(true);
+  const handleClose = () => setOpenResetPasswordModal(false);
 
   return (
     <>
@@ -142,15 +156,6 @@ function Profile() {
               readOnly: true,
             }}
           />
-
-          {/* <TextField id="cccd" label="CCCD / CMND / Hộ chiếu" />
-
-      <TextField id="invoice-info" label="Thông tin xuất hoá đơn" />
-
-      <TextField id="tax-code" label="Mã số thuế" /> */}
-
-          {/* <TextField id="favorite-category" label="Danh mục yêu thích" /> */}
-
           <TextField
             id="birthday"
             label="Ngày, tháng, năm sinh"
@@ -162,11 +167,88 @@ function Profile() {
             }}
           />
 
-          <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+          <Button
+            variant="contained"
+            type="submit"
+            color="primary"
+            sx={{ mt: 2 }}
+          >
             Lưu thay đổi
+          </Button>
+
+          <Button
+            variant="contained"
+            onClick={handleOpen}
+            color="error"
+            sx={{ mt: 2 }}
+          >
+            Đổi mật khẩu
           </Button>
         </Box>
       </Box>
+      <Modal
+        keepMounted
+        open={openResetPasswordModal}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={resetPasswordModalStyle}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Đổi mật khẩu
+          </Typography>
+          <Box
+            component="form"
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+
+              maxWidth: "1600px",
+              alignItems: "center",
+              justifyContent: "center",
+              margin: "30px 0",
+              width: "100%",
+            }}
+            noValidate
+            autoComplete="off"
+            onSubmit={handleSubmit}
+          >
+            <TextField
+              required
+              id="current-password"
+              label="Mật khẩu hiện tại"
+              fullWidth={true}
+              type="password"
+              sx={{
+                margin: "10px 0"
+              }}
+            />
+            <TextField
+              required
+              id="new-password"
+              label="Mật khẩu mới"
+              fullWidth={true}
+              type="password"
+              sx={{
+                margin: "10px 0"
+              }}
+            />
+            <TextField
+              required
+              id="new-password-confirm"
+              label="Xác nhận mật khẩu mới"
+              fullWidth={true}
+              type="password"
+              sx={{
+                margin: "10px 0"
+              }}
+            />
+            <Button variant="contained" color="error" sx={{ mt: 2 }}>
+              Đổi mật khẩu
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
     </>
   );
 }
