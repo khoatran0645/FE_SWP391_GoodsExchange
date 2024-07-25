993367439849aeac6ea1ed50b2cbf716955d6b72
REICHAN\phuon <thaottpse173309@fpt.edu.vn>
2024-07-25 22:47:05 +0700

    update Mod page

--- src/features/moderator/ManageReports.jsx
+++ src/features/moderator/ManageReports.jsx
@@ -1,44 +1,56 @@
 import * as React from "react";
-import ModeratorPage from "./ModeratorPage";
-import { Box, Button, Paper, Typography } from "@mui/material";
-import NavBarMo from "./NavBarMo";
-import useStore from "../../app/store";
-import Pagination from "@mui/material/Pagination";
-import Stack from "@mui/material/Stack";
+import {
+  Box,
+  Button,
+  Paper,
+  Typography,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Pagination,
+  Stack,
+} from "@mui/material";
 import { toast } from "react-toastify";
+import NavBarMo from "./NavBarMo";
+import ModeratorPage from "./ModeratorPage";
+import useStore from "../../app/store";
 
 export default function ManageReports() {
   const [page, setPage] = React.useState(1);
+  const [totalPage, setTotalPage] = React.useState(1);
+  const [listReport, setListReport] = React.useState([]);
+  const [openDialog, setOpenDialog] = React.useState(false);
+  const [selectedReport, setSelectedReport] = React.useState(null);
+  const [actionType, setActionType] = React.useState("");
+
   const getAllReports = useStore((state) => state.getAllReports);
   const approveReport = useStore((state) => state.approveReport);
   const denyReport = useStore((state) => state.denyReport);
 
-  const [totalPage, setTotalPage] = React.useState(1);
-
-  const [listReport, setListReport] = React.useState([]);
   const fetchData = async () => {
     await getAllReports(page, 10);
     const reportList = useStore.getState().reportList;
     console.log("report", reportList);
-    // setTotalPage(reportList.totalPage);
+    setTotalPage(reportList.totalPage);
     setListReport(reportList.data.items);
   };
+
   React.useEffect(() => {
     fetchData();
   }, [page]);
 
-  const handleApprove = async (item) => {
-    await approveReport(item);
+  const handleApprove = async () => {
+    await approveReport(selectedReport);
     const response = useStore.getState().response;
-    // setListReport(
-    //   listReport.filter((iter) => iter.productId !== item.productId)
-    // );
     fetchData();
     toast.success("You've successfully approved");
+    handleCloseDialog();
   };
 
-  const handleDeny = async (item) => {
-    await denyReport(item);
+  const handleDeny = async () => {
+    await denyReport(selectedReport);
     const response = useStore.getState().response;
     if (response?.data) {
       fetchData();
@@ -46,10 +58,31 @@ export default function ManageReports() {
     } else {
       toast.error("Error");
     }
+    handleCloseDialog();
   };
 
   const handlePageChange = (event, value) => {
     setPage(value);
+  };
+
+  const handleOpenDialog = (item, action) => {
+    setSelectedReport(item);
+    setActionType(action);
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+    setSelectedReport(null);
+    setActionType("");
+  };
+
+  const handleConfirmAction = () => {
+    if (actionType === "approve") {
+      handleApprove();
+    } else if (actionType === "deny") {
+      handleDeny();
+    }
   };
 
   return (
@@ -69,7 +102,6 @@ export default function ManageReports() {
         {listReport?.map((item, index) => (
           <Paper key={index} sx={{ p: 3, mb: 2, minWidth: "500px" }}>
             <Typography variant="h6">{item.reason}</Typography>
-            {/* <Typography variant="body1">{item.productId}</Typography> */}
             <Typography variant="body1">
               {item.reportMade} reported {item.reportReceived} for product name:{" "}
               {item.productName}
@@ -85,18 +117,14 @@ export default function ManageReports() {
                 variant="contained"
                 color="primary"
                 sx={{ mr: 1 }}
-                onClick={() => {
-                  handleApprove(item);
-                }}
+                onClick={() => handleOpenDialog(item, "approve")}
               >
                 Ban
               </Button>
               <Button
                 variant="contained"
                 color="secondary"
-                onClick={() => {
-                  handleDeny(item);
-                }}
+                onClick={() => handleOpenDialog(item, "deny")}
               >
                 Deny
               </Button>
@@ -115,6 +143,27 @@ export default function ManageReports() {
           />
         </Stack>
       </Box>
+
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogTitle>
+          {actionType === "approve" ? "Ban Confirmation" : "Deny Confirmation"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {actionType === "approve"
+              ? "Are you sure you want to ban this report?"
+              : "Are you sure you want to deny this report?"}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            No
+          </Button>
+          <Button onClick={handleConfirmAction} color="primary" autoFocus>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
