7f51dde98eace84408eade7f2f864cc5f4d1171a
quoctrieu010903 <luongquoctrieu165@gmail.com>
2024-07-27 13:28:02 +0700

    update trang profile

--- src/constant.js
+++ src/constant.js
@@ -22,8 +22,8 @@ export const API_SEARCH_PRODUCTS_FOR_USER =
   "/products/all?PageIndex=1&PageSize=10&ProductName={keyword}&IsApproved=true&IsActive=true";
 export const API_GET_PRODUCT_BY_ID = "/products/{id}";
 export const API_GET_PRODUCT_SELLER =
-  "/products/seller/all?PageIndex=1&PageSize=10";
-export const API_UPDATE_PRODUCT = "/products/update"
+  "products/seller/all?PageIndex=1&PageSize=10";
+export const API_UPDATE_PRODUCT = "/products/update";
 //! DANGER
 export const API_DELETE_PRODUCT = "/products/id?id={id}";
 
---
+++ src/features/profile/RequestTrade.jsx
@@ -0,0 +1,150 @@
+import React from "react";
+import useStore from "../../app/store";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Typography,
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActionArea,
+} from "@mui/material";
+import PersonIcon from "@mui/icons-material/Person";
+import GradeIcon from "@mui/icons-material/Grade";
+import ProductImage from "./productImage";
+
+const RequestTrade = () => {
+  const getSellerProduct = useStore((state) => state.getSellerProduct);
+
+  useEffect(() => {
+    getSellerProduct();
+  }, []);
+
+  const sellerProductList = useStore((state) => state.sellerProductList);
+
+  console.log("sellerProductList: ", sellerProductList?.data.items);
+  console.log(
+    "sellerProductList: ",
+    sellerProductList?.data.items.productImageUrl
+  );
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align="center" colSpan={2}>
+                <Typography variant="h6">Trade Details</Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Currently User Product</TableCell>
+              <TableCell>Sender's Product</TableCell>
+              <TableCell>Action</TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {sellerProductList?.data?.items.length > 0 ? (
+              sellerProductList?.data?.items.map((item) => (
+                <TableRow key={item.productId}>
+                  {/* Currently User Product Image and Name */}
+                  <TableCell>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                      }}
+                    >
+                      <CardMedia
+                        component="img"
+                        height="120" // Adjust as needed
+                        width="120" // Adjust as needed
+                        image={`${item.productImageUrl}?w=50&h=50s&fit=crop&auto=format`}
+                        alt={item.productName}
+                        sx={{ objectFit: "contain", borderRadius: "8px" }}
+                      />
+                      <Typography
+                        variant="body2"
+                        sx={{
+                          whiteSpace: "nowrap",
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                          textAlign: "center",
+                          mt: 1,
+                        }}
+                      >
+                        {item.productName}
+                      </Typography>
+                    </Box>
+                  </TableCell>
+
+                  {/* Sender's Product Image and Name */}
+                  <TableCell>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                      }}
+                    >
+                      <CardMedia
+                        component="img"
+                        height="120px" // Adjust as needed
+                        image={`${item.productImageUrl}?w=50&h=50&fit=crop&auto=format`}
+                        alt={item.productName}
+                        sx={{ objectFit: "contain", borderRadius: "8px" }}
+                      />
+                      <Typography
+                        variant="body2"
+                        sx={{
+                          whiteSpace: "nowrap",
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                          textAlign: "center",
+                          mt: 1,
+                        }}
+                      >
+                        {item.productName}
+                      </Typography>
+                    </Box>
+                  </TableCell>
+                  <TableCell>
+                    <Typography
+                      variant="body2"
+                      sx={{
+                        whiteSpace: "nowrap",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        textAlign: "center",
+                      }}
+                    >
+                      {item.userUpload}
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={2} align="center">
+                  <Typography variant="h6">No Products</Typography>
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
+
+export default RequestTrade;
